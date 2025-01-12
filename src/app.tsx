09eac86b88977647bf9adf9712974b11f359790c
mojtaba <darzimojtaba@gmail.com>
2025-01-12 18:57:09 +0330

    feat(appfile): add router, error boundry & lazy

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import './index.css';
+import { ErrorBoundary, PageLoading } from './components';
+
+const HomePage = React.lazy(() => import("./pages/home-page"))
+const PaymentDetailsPage = React.lazy(() => import("./pages/payment-details-page"))
+const NotFound = React.lazy(() => import("./pages/not-found"))
 
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="p-4">
-        <h1 className="text-2xl font-bold">Hello, React Query with Tailwind!</h1>
-      </div>
-    </QueryClientProvider>
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Suspense fallback={<PageLoading />}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/payments/:id" element={<PaymentDetailsPage />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ErrorBoundary>
+
   );
 };
 
